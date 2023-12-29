2b427303186767f4904750c095a897689569c2be
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-12-29 04:53:30 +0100

    App| Register MassTransit & config to AuctionService & SearchService

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,6 @@
 using AuctionService;
 using AuctionService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,24 @@ builder.Services.AddDbContext<AuctionDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMassTransit(
+    x =>
+    {
+        x.AddEntityFrameworkOutbox<AuctionDbContext>(opt =>
+        {
+            opt.QueryDelay = TimeSpan.FromSeconds(10);
+            opt.UsePostgres();
+            opt.UseBusOutbox();
+        });
+        x.UsingRabbitMq(
+            (context, cfg) =>
+            {
+                cfg.ConfigureEndpoints(context);
+            }
+        );
+    }
+);
 
 
 var app = builder.Build();
