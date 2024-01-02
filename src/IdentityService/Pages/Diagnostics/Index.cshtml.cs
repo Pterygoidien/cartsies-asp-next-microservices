6b6da4810793ae8567ce9404ae5fd7288e577098
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-02 10:30:04 +0100

    Infra| Adjustement for docker services

--- src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
+++ src/IdentityService/Pages/Diagnostics/Index.cshtml.cs
@@ -10,17 +10,17 @@ namespace IdentityService.Pages.Diagnostics;
 public class Index : PageModel
 {
     public ViewModel View { get; set; }
-        
+
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+        var localAddresses = new string[] { "::ffff:10.5.0.1", "::ffff:10.5.0.8", "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
         if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
         {
             return NotFound();
         }
 
         View = new ViewModel(await HttpContext.AuthenticateAsync());
-            
+
         return Page();
     }
 }
