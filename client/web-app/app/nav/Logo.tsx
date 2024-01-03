7556afaf657d3a6575c99ac0d8af65fbe953131e
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-03 15:26:34 +0100

    UI| Layout refactoring (moving Logo out of navbar component)

--- client/web-app/app/nav/Logo.tsx
+++ client/web-app/app/nav/Logo.tsx
@@ -1,23 +1,23 @@
 'use client'
 
-// import { useParamsStore } from '@/hooks/useParamsStore'
+import { useParamsStore } from '@/hooks/useParamsStore'
 import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
-// import { AiOutlineCar } from 'react-icons/ai'
+import { AiOutlineCar } from 'react-icons/ai'
 
 export default function Logo() {
     const router = useRouter();
     const pathname = usePathname();
-    // const reset = useParamsStore(state => state.reset);
+    const reset = useParamsStore(state => state.reset);
 
     function doReset() {
         if (pathname !== '/') router.push('/');
-        //reset();
+        reset();
     }
 
     return (
         <div onClick={doReset} className='cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500'>
-            {/* <AiOutlineCar size={34} /> */}
+           <AiOutlineCar size={34} /> 
             <div>Carsties Auctions</div>
         </div>
     )
