842332125b0613db0cbb3725e3cafcbca6009909
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-03 15:27:26 +0100

    UI| Add Search bar in navigation

--- client/web-app/app/nav/Search.tsx
+++ client/web-app/app/nav/Search.tsx
@@ -1,46 +1,30 @@
 'use client'
-
 import { useParamsStore } from '@/hooks/useParamsStore';
-import { usePathname, useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import { useEffect, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
-
-export default function Search() {
-    const router = useRouter();
-    const pathname = usePathname();
+export default function Search(): JSX.Element {
+    
     const setParams = useParamsStore(state => state.setParams);
-    const setSearchValue = useParamsStore(state => state.setSearchValue);
-    const searchValue = useParamsStore(state => state.searchValue);
+    const [value, setValue] = useState('');
 
-    function onChange(event: any) {
-        setSearchValue(event.target.value);
+    function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>):void {
+        setValue(e.target.value);
     }
-
-    function search() {
-        if (pathname !== '/') router.push('/');
-        setParams({searchTerm: searchValue});
+    function search(): void {
+        setParams({searchTerm: value});
     }
 
     return (
         <div className='flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm'>
             <input
-                onKeyDown={(e: any) => {
-                    if (e.key === 'Enter') search();
-                }}
-                value={searchValue}
-                onChange={onChange}
+                onChange={onChangeHandler}
+                onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' && search()}
                 type="text"
-                placeholder='Search for cars by make, model or color'
-                className='
-                input-custom
-                text-sm
-                text-gray-600
-            '
+                placeholder="Search for cars by make, model or color"
+                className="flex-grow pl-5 bg-transparent focus:outline-none border-transparent focus:border-transparent focus:ring-0 text-sm text-gray-600 placeholder-gray-400"
             />
-            <button onClick={search}>
-                <FaSearch
-                    size={34}
-                    className='bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2' />
+            <button onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => search()}>
+                <FaSearch size={35} className='bg-red-400 text-white rounded-full p-2 cursor-pointer mx-2' />
             </button>
         </div>
     )
