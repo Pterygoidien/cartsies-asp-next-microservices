01f76352f1e7026d0934ed855a586c5eabb606a5
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-03 15:26:58 +0100

    UI| auctionAction : minor refactoring, clear blank lines, etc.

--- client/web-app/app/actions/auctionActions.ts
+++ client/web-app/app/actions/auctionActions.ts
@@ -1,21 +1,14 @@
 "use server";
 
 import { PagedResult } from "@/types";
-
 /**
- * Fetches auctions from the server
- * @param currentPage
- * @param pageSize
- * @returns {Promise<PagedResult<IAuction>>}
+ * Fetches data from the server based on the provided query.
+ * @param query - The query string to be appended to the URL.
+ * @returns A promise that resolves to a PagedResult of IAuction objects.
+ * @throws An error if the fetch request fails.
  */
-
-export async function getData(
-  currentPage: number,
-  pageSize: number
-): Promise<PagedResult<IAuction>> {
-  const res = await fetch(
-    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${currentPage}`
-  );
+export async function getData(query: string): Promise<PagedResult<IAuction>> {
+  const res = await fetch(`http://localhost:6001/search${query}`);
   if (!res.ok) throw new Error("Could not fetch auctions");
   const auctions: PagedResult<IAuction> = await res.json();
   return auctions;
