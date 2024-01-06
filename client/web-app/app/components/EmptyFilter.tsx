92008a7d550c0a75614c91a55a9808fc783351c6
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-06 14:11:39 +0100

    UI| Add functionality to EmptyFilter to log in for protected routes

--- client/web-app/app/components/EmptyFilter.tsx
+++ client/web-app/app/components/EmptyFilter.tsx
@@ -1,17 +1,24 @@
+'use client';
+
 import { useParamsStore } from "@/hooks/useParamsStore";
 import Heading from "./Heading";
 import { Button } from "flowbite-react";
+import { signIn } from "next-auth/react";
 
 type Props = {
     title?: string;
     subtitle?: string;
     showReset?: boolean;
+    showLogin?: boolean;
+    callbackUrl?: string;
 }
 
 export default function EmptyFilter({ 
     title = 'No results found', 
     subtitle = 'Try adjusting your search criteria', 
-    showReset
+    showReset,
+    showLogin,
+    callbackUrl
 }: Props): JSX.Element {
     
     const reset = useParamsStore(state => state.reset);
@@ -22,6 +29,9 @@ export default function EmptyFilter({
                 {showReset && (
                     <Button outline onClick={reset}>Reset filters</Button>
                 )}
+                {showLogin && (
+                    <Button outline onClick={()=>signIn('id-server', {callbackUrl})}>Login</Button>
+                 )}
             </div>
     </div>
    
