4cc325b1a28824926f2aeee6a7eddc713182a1cf
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-06 14:12:29 +0100

    UI| UserActions navbar component : add Dropdown menu for logged in users

--- client/web-app/app/nav/UserActions.tsx
+++ client/web-app/app/nav/UserActions.tsx
@@ -1,10 +1,43 @@
-import { Button } from "flowbite-react";
+'use client';
+import { Dropdown } from "flowbite-react";
+import { User } from "next-auth";
+import { signOut } from "next-auth/react";
 import Link from "next/link";
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from "react-icons/ai";
+import { HiCog, HiUser } from "react-icons/hi";
 
-export default function UserActions() {
+type Props = {
+  user: Partial<User>
+}
+
+export default function UserActions({ user }: Props) {
   return (
-      <Button outline>
-        <Link href='/session'>Session</Link>
-      </Button>
+    <Dropdown inline label={`Welcome ${user.name}`}>
+      <Dropdown.Item icon={HiUser}>
+        <Link href="/">
+          My Auctions
+        </Link>
+      </Dropdown.Item>
+
+      <Dropdown.Item icon={AiFillTrophy}>
+        <Link href="/">
+          Auctions wons
+        </Link>
+      </Dropdown.Item>
+     <Dropdown.Item icon={AiFillCar}>
+        <Link href="/">
+         Sell my car
+        </Link>
+      </Dropdown.Item>
+      <Dropdown.Item icon={HiCog}>
+        <Link href="/session">
+          Session (dev only)
+        </Link>
+      </Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item icon={AiOutlineLogout} onClick={()=>signOut({callbackUrl:'/'})}>
+          Log out
+      </Dropdown.Item>
+      </Dropdown>
   )
 }
