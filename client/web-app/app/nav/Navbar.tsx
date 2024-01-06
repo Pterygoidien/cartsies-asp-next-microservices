4bf5287584d3384aef60e704ea5ddb0bc541e9b9
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2024-01-06 11:57:16 +0100

    UI| Navbar : import Login UI button

--- client/web-app/app/nav/Navbar.tsx
+++ client/web-app/app/nav/Navbar.tsx
@@ -1,13 +1,22 @@
 import { AiOutlineCar } from 'react-icons/ai';
 import Search from './Search';
 import Logo from './Logo';
+import LoginButton from './LoginButton';
+import { getCurrentUser } from '../actions/authActions';
+import UserActions from './UserActions';
 
-export default function Navbar(): JSX.Element {
+export default async function Navbar(): Promise<JSX.Element> {
+
+  const user = await getCurrentUser();
+
   return (
     <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
       <Logo />
       <Search />
-      <div>Login</div>
+      {user ? 
+        (<UserActions />) : (<LoginButton />)
+      }
+     
     </header>   
   )
 }
